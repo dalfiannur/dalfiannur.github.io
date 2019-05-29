c36da5c3cffca130eee1d995298052a466fcbddf
dalfiannur <dalfiannur@gmail.com>
2019-05-29 19:48:15 +0700

    1

---
+++ precache-manifest.5b00028ffa6a99e8f3da7812654626cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95df42a07f818c2e74c2f71a8e90f1bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dff50feaa6494327631",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0e420720a0804ec66d06",
+    "url": "/static/js/2.c4518926.chunk.js"
+  },
+  {
+    "revision": "0dff50feaa6494327631",
+    "url": "/static/js/main.bd3e9281.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
